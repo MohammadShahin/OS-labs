f71bf9b5bd94f9a6d2ed7dbb58cd588ef588fec7
MohammadShahin <59062739+MohammadShahin@users.noreply.github.com>
2020-11-04 22:55:37 +0300

    Update ex1.c

--- week12/ex1.c
+++ week12/ex1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+#define SIZE 20
+
+int main(){
+	FILE *random = open("/dev/random", O_RDONLY);
+	if (random < 0){
+	    return -1;
+	}
+    char array[SIZE + 1];
+    int res = read(random, array, SIZE);
+    if (res < 0){
+        close(random);
+        return -1;
+    }
+	printf("\n");
+	close(random);
+	return 0;
+}
